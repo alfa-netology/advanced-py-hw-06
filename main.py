9489f3a181cf10a1f7cedc123044e89a1845f02e
Alexsander Uglin <uglin76@mail.ru>
2021-05-04 00:42:20 +0300

    refactoring

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def main():
     find_keywords_in_preview = ()
 
     for article in articles:
-        article_date = utilites.date_beautify(article.find('span', class_='post__time').text)
-        artricle_link = article.find('a', class_='post__title_link').get('href')
-        article_title = article.find('a', class_='post__title_link').text
-        article_preview = article.find('div', class_='post__text').text.strip()
+        date = utilites.date_beautify(article.find('span', class_='post__time').text)
+        link = article.find('a', class_='post__title_link').get('href')
+        title = article.find('a', class_='post__title_link').text
+        preview = article.find('div', class_='post__text').text.strip()
 
-        if re.search(keywords_pattern, article_preview, flags=re.I):
-            find_keywords_in_preview += (f"<{article_date}> - <{article_title}> - <{artricle_link}>",)
+        if re.search(keywords_pattern, preview, flags=re.I):
+            find_keywords_in_preview += (f"<{date}> - <{title}> - <{link}>",)
 
     print(*find_keywords_in_preview, sep='\n')
 
